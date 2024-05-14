268b83723d614e3be85eda5c55f25b3f5028095e
Reyshyram <169070816+HctrNvt@users.noreply.github.com>
2024-05-14 17:40:20 +0200

    "rzrzerrze

--- monstre.py
+++ monstre.py
@@ -1,4 +1,12 @@
 class Monstre():
-    def __init__(self) -> None:
-        
+    def __init__(self,coordonnees: tuple) -> None:
+        self.coordonnees = coordonnees
+        self.health = 250
+        self.max_health = self.health
+        self.attack = 25
+        self.all_projectiles = pygame.sprite.Group()
+        self.velocity = 2
+        # le self velocity correspond a la vitesse de deplacement en pixel
+        self.image = pygame.image.load('assets/monstre.png')
+        self.rect = self.image.get_rect()
         pass
